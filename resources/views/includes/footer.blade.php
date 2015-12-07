69105a80bff10a48b085a5457600354093c77209
Mahdi Bagheri <mb.programmer@yahoo.com>
2015-12-07 18:07:03 +0330

    new home page design

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,1 +1,8 @@
-<footer></footer>
+<footer class="container">
+    <hr>
+    <div class="row">
+        <div class="col-lg-10">
+            <p class="text-center">استفاده با ذکر منبع بلامانع است.</p>
+        </div>
+    </div>
+</footer>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,22 @@
-<header>
-    وبلاگ
+<header class="bg-info">
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">{{env('APP_NAME', 'وبلاگ')}}</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">درباره</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
