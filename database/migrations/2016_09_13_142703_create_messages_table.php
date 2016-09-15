ac08960bd9179edbb0c7766299419ea973e112a5
Abbas Shakiba <shakiba_9@yahoo.com>
2016-09-15 15:22:46 +0430

    Add tracking system's database

---
+++ database/migrations/2016_09_13_142703_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ticket_id');
+            $table->string('body');
+            $table->integer('created_at');
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('messages');
+    }
+}
---
+++ database/migrations/2016_09_14_143909_create_ticket_status_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_status', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ticket_id');
+            $table->enum('status', ['new', 'assigned', 'closed']);
+            $table->integer('user_id');
+            $table->integer('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ticket_status');
+    }
+}
