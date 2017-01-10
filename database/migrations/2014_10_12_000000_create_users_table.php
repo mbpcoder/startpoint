dac98d4eb4e0df19429bf11f246570162354e0d9
Mahdi Bagheri <mb.programmer@yahoo.com>
2017-01-10 23:51:43 +0330

    reformat users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,14 +17,14 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name', 100);
             $table->string('email', 50)->unique();
-            $table->string('password', 60);
             $table->string('mobile', 15)->nullable();
             $table->boolean('disabled')->defualt(false);
             $table->string('description', 1000);
+            $table->string('password', 60);
             $table->rememberToken();
+            $table->boolean('is_admin')->default(0);
             $table->integer('created_at');
             $table->integer('updated_at');
-            $table->boolean('is_admin')->default(0);
         });
     }
 
