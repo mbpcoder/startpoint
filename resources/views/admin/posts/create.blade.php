34a585f057bba27e6f72c0dee99d55ac98a657b9
Mahdi Bagheri <mb.programmer@yahoo.com>
2017-01-10 15:33:44 +0330

    style improvement of admin create post

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,54 +4,46 @@
     <script src="{{asset('/js/vendors/summernote.min.js')}}"></script>
     <div class="container">
         <form class="form form-horizontal" method="post" action="/admin/posts">
+            <input type="hidden" value="{!! csrf_token() !!}" name="_token">
             <fieldset>
                 <legend>
                     <h3>افزودن مطلب</h3>
                 </legend>
-                {{-- Error messages --}}
-                @if ( $errors->count() > 0 )
-                    <div class="alert alert-danger " role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <p>خطاهای زیر رخ داده است:</p>
-                        <ul>
-                            @foreach( $errors->all() as $message )
-                                <li>{{ $message }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
-
-                <input type="hidden" value="{!! csrf_token() !!}" name="_token">
-
                 <div class="row">
-                    <div class="col-lg-8">
-                        <div class="form-group">
-                            <label for="title" class="control-label col-lg-4 required-input">عنوان مطلب</label>
+                    <div class="col-md-10">
+                        <div class="form-group required {{$errors->has('title') ? 'has-error' : ''}}">
+                            <label for="title" class="control-label col-md-3">عنوان</label>
 
-                            <div class="col-lg-8">
-                                <input type="text" value="{{old('title')}}" class="form-control" id="title" name="title">
+                            <div class="col-md-7">
+                                <input type="text" value="{{old('title')}}" class="form-control" id="title"
+                                       name="title">
+                                @if($errors->has('title'))
+                                    <span class="help-block">{{$errors->first('title')}}</span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group {{$errors->has('alias') ? 'has-error' : ''}}">
+                            <label for="alias" class="control-label col-md-3">نام مستعار</label>
+
+                            <div class="col-md-7">
+                                <input type="text" value="{{old('alias')}}" class="form-control" id="alias"
+                                       name="alias">
+                                @if($errors->has('alias'))
+                                    <span class="help-block">{{$errors->first('alias')}}</span>
+                                @endif
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="alias" class="control-label col-lg-4 required-input">نام مستعار</label>
+                            <label for="published" class="control-label col-md-3">منتشر شده</label>
 
-                            <div class="col-lg-8">
-                                <input type="text" value="{{old('alias')}}" class="form-control" id="alias" name="alias">
+                            <div class="col-md-7">
+                                <input type="checkbox" class="checkbox-inline" id="published" name="published">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="published" class="control-label col-lg-4 required-input">منتشر شده</label>
+                            <label for="categories" class="control-label col-md-3 required-input">دسته بندی ها</label>
 
-                            <div class="col-lg-8">
-                                <input type="checkbox" class="form-control" id="published" name="published">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="categories" class="control-label col-lg-4 required-input">دسته بندی ها</label>
-
-                            <div class="col-lg-8">
+                            <div class="col-md-7">
                                 <select multiple class="form-control" id="categories" name="categories[]">
                                     <?php $first = true ?>
                                     @foreach(\StartPoint\Category::all() as $category)
@@ -68,21 +60,28 @@
                     </div>
                 </div>
 
-                <div class="form-group">
-                    <label for="Body">محتوی</label>
-                    <textarea type="text" class="form-control" id="Body" name="body" rows="20"></textarea>
+                <div class="form-group required {{$errors->has('body') ? 'has-error' : ''}}">
+                    <label class="control-label" for="Body">محتوی</label>
+                    <textarea type="text" class="form-control" id="Body" name="" rows="20">{{old('body')}}</textarea>
+                    @if($errors->has('body'))
+                        <span class="help-block">{{$errors->first('body')}}</span>
+                    @endif
                 </div>
 
-                <hr>
-                <div class="form-group">
-                    <button type="submit" class="btn btn-success">
-                        <i class="fa fa-save"></i>
-                        ذخیره
-                    </button>
-                    <a href="/admin/posts" class="btn btn-danger">
-                        <i class="fa fa-times-circle-o"></i>
-                        انصراف
-                    </a>
+
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="form-group">
+                            <div class="col-md-7 col-md-offset-3 text-left">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-save"></i> ذخیره
+                                </button>
+                                <a href="/admin/posts" class="btn btn-default">
+                                    <i class="fa fa-times-circle-o"></i> انصراف
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </fieldset>
         </form>
